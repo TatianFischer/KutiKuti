8d29f16966da611419b51cf2fbf967a0ef170071
benji123456 <benjaminbaloul@hotmail.com>
2017-02-20 17:21:50 +0100

    creation du master.blade.php
    version 2

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Kuti Kuti</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="{{URL::asset('css/style.css')}}" rel="stylesheet">
-</head>
-<body>
-    @include('layouts.nav')
-    
-    <!-- Page Content -->
-    <div class="container-fluid" id="main">
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid" id="main">
         
         <div class="logo">
             <img src="img/logo.png">
@@ -62,18 +52,6 @@
     </div>
     <!-- /.container -->
 
-    <div class="container">
+@endsection
 
-        <hr>
-
-        <!-- Footer -->
-      @include('layouts.footer')
-
-    </div>
-    <!-- /.container -->
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</body>
-</html>
+   
