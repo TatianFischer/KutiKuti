3c2a6509562fe4e74f7f9d01304fe14ab942d957
TatianFischer <tatiana.fischer@hotmail.fr>
2017-03-16 19:17:15 +0100

    Mise ne page du formulaire

--- public/js/preorder.js
+++ public/js/preorder.js
@@ -1,5 +1,5 @@
 $(function(){
-    // Affichage des détails
+    // Affichage des détails de précommande dans l'admin
 	$('.details').on('submit', function(e){
 		e.preventDefault();
 		
@@ -56,8 +56,73 @@ $(function(){
 	})
 
 
+    // Formulaire en plusieurs étapes
+    var steps = $('#form_preco fieldset');
+    var count = steps.length;
+    $('#save_preco').hide();
+    $('#form_preco').before('<ul id="steps" class="nav nav-pills nav-justified"></ul>');
 
-    // Code Poste
+    steps.each(function(i){
+        $(this).wrap('<div id="step'+i+'"></div>');
+        $(this).append('<p id="step'+i+'commands"></p>'); // Ajout des commandes
+
+        // 2
+        var name = $(this).find("legend").html();
+        $("<li>").attr('id', "stepDesc" + i)
+            .append($('<span>').text("Etape " + (i + 1) + " " + name))
+                .appendTo("#steps");
+        
+
+        if(i == 0){
+            createNextButton(i);
+            selectStep(i);
+        } else if(i == count-1){
+            $('#step'+i).hide();
+            createPrevButton(i);
+        } else {
+            $('#step'+i).hide();
+            createPrevButton(i);
+            createNextButton(i);
+        }
+    })
+
+    function createPrevButton(i) {
+        var stepName = "step" + i;
+        $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Prev' class='prev btn btn-primary'><span class='glyphicon glyphicon-chevron-left'> Précédent</a>");
+
+        $("#" + stepName + "Prev").on("click", function(e) {
+            $("#" + stepName).hide();
+            $("#step" + (i - 1)).show();
+            $('#save_preco').hide();
+            selectStep(i - 1);
+        });
+    }
+
+    function createNextButton(i) {
+        var stepName = "step" + i;
+        $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next btn btn-primary'>Suivant <span class='glyphicon glyphicon-chevron-right'></a>");
+
+        $("#" + stepName + "Next").on("click", function(e) {
+            $("#" + stepName).hide();
+            $("#step" + (i + 1)).show();
+            if (i + 2 == count)
+                $('#save_preco').show();
+            selectStep(i + 1);
+        });
+    }
+
+    function selectStep(i) {
+        $("#steps li").removeClass("current");
+        $("#stepDesc" + i).addClass("current");
+    }
+
+
+
+
+
+
+
+    // Code Postal
     // https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&q=94230&facet=nom_de_la_commune&facet=code_postal
     $('input[name="cp"]').on('keyup', function(){
         var min_length = 2;
@@ -66,7 +131,7 @@ $(function(){
         if(keyword.length >= min_length){
             $.ajax({
                 method: "GET",
-                url: "https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&q="+$(this).val()+"&rows=5&facet=nom_de_la_commune&facet=code_postal",
+                url: "https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&sort=nom_de_la_commune&row=45&q="+$(this).val()+"&facet=nom_de_la_commune&facet=code_postal",
                 dataType: "json",
 
                 success: function(data){
@@ -75,7 +140,7 @@ $(function(){
                     // Récupération et affichage des données
                     $.each(data.records, function(index, value){
                         $('<li>')
-                            .append($('<a>').addClass('cp_option').html('<span class="select_city">'+value.fields.nom_de_la_commune+'</span> (<span class="select_cp">'+value.fields.code_postal+'</span>)')
+                            .append($('<a>').addClass('cp_option').html('<span class="select_city">'+value.fields.nom_de_la_commune.toLowerCase()+'</span> (<span class="select_cp">'+value.fields.code_postal+'</span>)')
                                 .on("click", function(){
                                     $('input[name="city"]').val($(this).children('span.select_city').text());
                                     $('input[name="cp"]').val($(this).children('span.select_cp').text());
@@ -99,6 +164,10 @@ $(function(){
 
     })
 
+    $('input[name="cp"]').on('blur', function(){
+        $('.cp_list').hide();
+    })
+
     $('input[name="city"]').on('keyup', function(){
         var min_length = 2;
         var keyword = $(this).val();
@@ -106,7 +175,7 @@ $(function(){
         if(keyword.length >= min_length){
             $.ajax({
                 method: "GET",
-                url: "https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&q="+$(this).val()+"&rows=5&facet=nom_de_la_commune&facet=code_postal",
+                url: "https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&sort=nom_de_la_commune&row=45&q="+$(this).val()+"&facet=nom_de_la_commune&facet=code_postal",
                 dataType: "json",
 
                 success: function(data){
@@ -115,7 +184,7 @@ $(function(){
                     // Récupération et affichage des données
                     $.each(data.records, function(index, value){
                         $('<li>')
-                            .append($('<a>').addClass('ciy_option').html('<span class="select_city">'+value.fields.nom_de_la_commune+'</span> (<span class="select_cp">'+value.fields.code_postal+'</span>)')
+                            .append($('<a>').addClass('city_option').html('<span class="select_city">'+value.fields.nom_de_la_commune+'</span> (<span class="select_cp">'+value.fields.code_postal+'</span>)')
                                 .on("click", function(){
                                 $('input[name="city"]').val($(this).children('span.select_city').text());
                                 $('input[name="cp"]').val($(this).children('span.select_cp').text());
@@ -137,6 +206,10 @@ $(function(){
             $('.city_list').hide();
         }
 
+    })
+
+    $('input[name="city"]').on('blur', function(){
+        $('.city_list').hide();
     })
 
 })
--- resources/views/preorder/create.blade.php
+++ resources/views/preorder/create.blade.php
@@ -1,101 +1,153 @@
 @extends ('layouts.master')
 
 @push('css')
-<link href='http://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet' type='text/css'>
+<!-- <link href='http://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet' type='text/css'> -->
 <link href="http://netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
 <link href="{{URL::asset('css/preco.css')}}" rel="stylesheet">
 @endpush
 
 @section('content')
 <div class="container">
-    @if(session('success'))
-        <div class="alert alert-success"> {{session('success')}} </div>
-    @endif
+    <div class="row">
+        @if(session('success'))
+            <div class="alert alert-success"> {{session('success')}} </div>
+        @endif
     
-    @if(isset($success))
-        <div class="alert alert-success"> {{$success}} </div>
-    @endif
+        @if(isset($success))
+            <div class="alert alert-success"> {{$success}} </div>
+        @endif
 
-    @if(count($errors))
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        @if(count($errors))
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
-    @endif
+
+    <div class="row" id="content">       
+        
+        <div class="col-xs-12">
+            <h1>Formulaire de précommande</h1>
+            <form class="form-horizontal" action="{{route('preorders.store')}}" method="post" autocomplete="on" id="form_preco">
+                <fieldset>
+                    <legend>Informations</legend>
+                    <p class="indication">
+                        Les champs avec une<span class="required"> * </span>sont obligatoires
+                    </p>
+                    <!-- CLIENTS -->
+                    <div class="col-xs-12" id="client">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="lastname" class="col-xs-4"> Nom <span class="required">*</span>
+                                </label>
+                                 <div class="col-xs-8">
+                                    <div class="input-group">
+                                        <span class="input-group-addon glyphicon glyphicon-user"></span>
+                                        <input class="form-control" type="text" name="lastname" id="lastname" required>
+                                    </div>
+                                </div>   
+                            </div>
+
+                            <div class="form-group">
+                                <label for="firstname" class="col-xs-4"> Prénom <span class="required">*</span></label>
+                                <div class="col-xs-8">
+                                    <div class="input-group">
+                                        <span class="input-group-addon glyphicon glyphicon-user"></span>
+                                        <input class="form-control" type="text" name="firstname" id="firstname" required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email" class="col-xs-4">Email <span class="required">*</span></label>
+                                <div class="col-xs-8">
+                                    <div class="input-group">
+                                        <span class="input-group-addon glyphicon glyphicon-envelope"></span>
+                                        <input class="form-control" type="email" name="email" id="email" required>
+                                    </div>
+                                </div>     
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="address" class="col-xs-4"> Adresse <span class="required">*</span></label>
+                                <div class="col-xs-8">
+                                    <div class="input-group">
+                                        <span class="input-group-addon glyphicon glyphicon-map-marker"></span>
+                                        <input class="form-control" type="text" name="address" id="address" required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cp" class="col-xs-4">Code Postal<span class="required">*</span></label>
+                                <div class="col-xs-8">
+                                    <div class="input-group">
+                                        <span class="input-group-addon glyphicon glyphicon-map-marker"></span>
+                                        <input class="form-control" type="text" name="cp" id="cp" required>
+                                    </div>
+                                    <ul class="cp_list" hidden></ul>
+                                </div>                        
+                            </div>
+
+                            <div class="form-group">
+                                <label for="city" class="col-xs-4">Ville <span class="required">*</span></label>
+                                <div class="col-xs-8">
+                                    <div class="input-group">
+                                        <span class="input-group-addon glyphicon glyphicon-map-marker"></span>
+                                        <input class="form-control" type="text" name="city" id="city" required>
+                                    </div>
+                                    <ul class="city_list" hidden></ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- Client -->
+                </fieldset>
+
+                <fieldset>                
+                    <legend>Commande</legend>
+                    {{ csrf_field() }}
+                    
+                    <div class="col-xs-12" id="commande">
+                        <div class="list-group">
+                            <div data-toggle="buttons">
+                                @foreach($products as $product)
+                                <label class="list-group-item btn col-md-3 col-xs-6 {{($product->modele != 'LapiKuti') ? 'item-middle' : ''}}" for="{{$product->modele}}">
+                                    <img src="{{URL::asset('img/products/'.$product->photo)}}" alt="{{$product->title}}">
+                                    <input type="checkbox" name="product[{{$product->id}}]" value="{{$product->id}}">
+                                    @if($product->modele != "LapiKuti") <br>  @endif
+                                    <p>{{$product->modele}} - {{$product->couleur}} - {{$product->price}} €</p>
+                                </label>
+                                    
+                                    <!-- <p>Quantité à commander :</p>
+                                    <input type="text" name="quantity[{{$product->id}}]"> -->
+                                @endforeach
+                            </div>
+                        </div>
+                    </div> <!-- Commande -->
+                </fieldset>
+
+                <fieldset>
+                    <legend>Résumé</legend>
+                    <div class="col-xs-12">
+                        <!-- lol -->
+                    </div>
+                </fieldset>
+ 
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-6">
+                        <input type="submit" value=" C'est parti ! " class="btn" id="save_preco"/>
+                    </div> 
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-
-<div id="content">
-    <h1>Formulaire de précommande</h1>
-
-    <form action="{{route('preorders.store')}}" method="post" autocomplete="on">
-      {{ csrf_field() }}
-        <div class="group">
-            <label for="lastname" class="icon-user"> Nom
-                <span class="required">*</span>
-            </label>
-            <input type="text" name="lastname" id="lastname" required="required" placeholder="Votre nom" />
-        </div>
-
-        <div class="group">
-            <label for="firstname" class="icon-home">Prenom</label>
-                <span class="required">*</span>
-            <input type="text" name="firstname" id="firstname" placeholder="Votre prenom" />
-        </div>
-
-        <div class="group">
-            <label for="email" class="icon-envelope"> email</label>
-                <span class="required">*</span>
-            <input type="email" name="email" id="email" placeholder="Entrez votre email" required="required" />
-        </div>
-
-        <div class="group">
-            <label for="address" class="icon-home"> Adresse
-                <span class="required">*</span>
-            </label>
-            <input type="text" name="address" id="address" placeholder="Entrez votre adresse" required="required" />
-        </div>
-
-        <div class="group">
-            <label for="cp" class="icon-envelope"> code postal
-                <span class="required">*</span>
-            </label>
-            <input type="text" name="cp" id="cp" placeholder="Entrez votre code postal" required="required" />
-            <ul class="cp_list"></ul>
-        </div>
-
-        <div class="group">
-            <label for="city" class="icon-envelope"> ville
-                <span class="required">*</span>
-            </label>
-            <input type="text" name="city" id="city" placeholder="Entrez votre ville" required="required" />
-            <ul class="city_list"></ul>
-        </div>
-
-        <hr>
-        <div class="produit_choix">
-          @foreach($products as $product)
-          <div class="group">
-              <label for=""> {{$product->modele}} - {{$product->couleur}} - {{$product->price}} € </label>
-                <img src="{{URL::asset('img/products/'.$product->photo)}}" alt="{{$product->title}}">
-                <input type="checkbox" name="product[{{$product->id}}]" value="{{$product->id}}">
-                <p>Quantité à commander :</p>
-                <input type="text" name="quantity[{{$product->id}}]">
-          </div>
-          @endforeach
-        </div>
-
-        <p class="indication">Les champs avec une
-            <span class="required"> * </span>sont obligatoires
-        </p>
-
-        <input type="submit" value=" C'est parti ! " />
-
-    </form>
-</div>
-
 
 @endsection
 
